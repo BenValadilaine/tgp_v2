f1d43c540c938a53d3eaa2f017b221990f89439c
Juliet <juliet.dr@tutanota.com>
2020-08-06 22:48:10 +0200

    flash added

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,14 +2,8 @@ class ProfilesController < ApplicationController
 
   def show
     loggedin?
-    puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-    p params[:id]
     @author = User.find_by(first_name: params[:id])
-    puts "########################################"
-    p @author
     @author_city = City.find(@author.city_id)
-    puts "==================================="
-    puts @author_city
   end
 
   def new
@@ -18,14 +12,11 @@ class ProfilesController < ApplicationController
 
   def create
     @user_city = City.create(name: params[:city], zip_code: 67000)
-    puts "*"*30
-    puts @user_city
-    if params[:password] == params[:password_confirm]
-      @user = User.create(first_name: params[:first_name], last_name: params[:last_name], age: params[:age], city: @user_city, email: params[:email], password: params[:password])
-      redirect_to root_path
+    @user = User.new(first_name: params[:first_name], last_name: params[:last_name], age: params[:age], city: @user_city, email: params[:email], password: params[:password])
+    if params[:password] == params[:password_confirm] && @user.save
+      redirect_to root_path :notice => "success"
     else
-      #penser à mettre un flash
-      render :action => "new"
+     redirect_to new_profile_path :notice => "failure"
     end
 
 
